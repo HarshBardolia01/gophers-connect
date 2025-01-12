c45c87cf50c08150746664bf8059967fdcb79d8e
HarshBardolia01 <harshbardolia02@gmail.com>
2025-01-12 11:12:54 +0530

    chore: added env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/HarshBardolia01/gophers-connect/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := application{
