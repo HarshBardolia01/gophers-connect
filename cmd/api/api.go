d2fa36ea0b751b02c3b1e4b156afed4468214d63
HarshBardolia01 <harshbardolia02@gmail.com>
2025-01-14 16:57:22 +0530

    feat: posts api

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -44,6 +44,14 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+
+			r.Route("/{postId}", func(r chi.Router) {
+				r.Get("/", app.getPostById)
+			})
+		})
 	})
 
 	return r
